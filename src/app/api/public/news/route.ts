fec2e7dee37a2726221756951dac0d590ef54d6f
Jacky Tran <tdhungit@gmail.com>
2023-09-08 17:12:24 +0700

    add pagination for app repository

--- src/app/api/public/news/route.ts
+++ src/app/api/public/news/route.ts
@@ -1,29 +1,21 @@
 import { articleCategoryRepository } from '@/repositories/ArticleCategoryRepository';
 import { articleRepository } from '@/repositories/ArticleRepository';
-import { createDocsResponse } from '@/utils/firestore';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
 	// all categories
 	const categories = await articleCategoryRepository.find();
 	// articles pagination
-	const perPage = 20;
-	const total = await articleRepository.count();
-	let offset = 0;
+	const page: any = req.nextUrl.searchParams.get('page') || 1;
+	const pagination = await articleRepository.pagination({
+		page: parseInt(page),
+		orderBy: [
+			{
+				field: 'publishAt',
+				direction: 'desc',
+			},
+		],
+	});
 
-	const currentPage = req.nextUrl.searchParams.get('page');
-	let page: number = currentPage ? parseInt(currentPage) : 1;
-	if (page > 1) {
-		offset = page * perPage;
-	}
-
-	const query = articleRepository.collectionRef
-		.orderBy('publishAt', 'desc')
-		.offset(offset)
-		.limit(perPage);
-
-	const snapshot = await query.get();
-	const articles = createDocsResponse(snapshot);
-
-	return NextResponse.json({ categories, total, articles });
+	return NextResponse.json({ categories, ...pagination });
 }
--- src/repositories/AppRepository.ts
+++ src/repositories/AppRepository.ts
@@ -1,5 +1,24 @@
 import { initDatabase } from '@/services/firebase';
+import { createDocsResponse } from '@/utils/firestore';
 import { BaseFirestoreRepository, IEntity, IRepository } from 'fireorm';
+
+interface OrderByQuery {
+	field: string;
+	direction?: any;
+}
+
+interface FilterQuery {
+	field: string;
+	operator: string;
+	value?: string;
+}
+
+interface PaginationQuery {
+	page?: number;
+	limit?: number;
+	orderBy?: OrderByQuery[];
+	filter?: FilterQuery[];
+}
 
 export class AppRepository<T extends IEntity>
 	extends BaseFirestoreRepository<T>
@@ -22,5 +41,38 @@ export class AppRepository<T extends IEntity>
 			snapshot = await this.collectionRef.count().get();
 		}
 		return snapshot.data().count;
+	}
+
+	async pagination({ page, limit, orderBy, filter }: PaginationQuery) {
+		page = page || 1;
+		if (page <= 0) page = 1;
+		limit = limit || 20;
+
+		const query = this.collectionRef;
+		// filter
+		if (filter && filter.length > 0) {
+			filter.map((item: any) => {
+				query.where(item.field, item.operator, item.value);
+			});
+		}
+		// get total
+		const snapshotCount = await query.count().get();
+		const total = snapshotCount.data().count;
+		// order by
+		if (orderBy && orderBy.length > 0) {
+			orderBy.map((item) => {
+				query.orderBy(item.field, item.direction || 'asc');
+			});
+		}
+		// offset
+		const offset = (page - 1) * limit;
+		// get snapshot
+		const snapshot = await query.offset(offset).limit(limit).get();
+		// create response
+		return {
+			docs: createDocsResponse(snapshot),
+			total,
+			page,
+		};
 	}
 }
